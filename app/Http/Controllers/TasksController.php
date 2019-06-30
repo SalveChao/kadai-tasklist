ba77056f4de50ab2147fccd526d5c9c0660324af
salve.chao <salve-chao@live.jp>
2019-06-30 06:35:53 +0000

    modifying controllers

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -28,7 +28,11 @@ class TasksController extends Controller
      */
     public function create()
     {
-        //
+        $task = new Task;
+        
+        return view('tasks.create', [
+            'task' => $task,
+            ]);
     }
 
     /**
@@ -39,7 +43,11 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $task = new Task;
+        $task->content = $request->content;
+        $task->save();
+        
+        return redirect('/');
     }
 
     /**
@@ -50,7 +58,10 @@ class TasksController extends Controller
      */
     public function show($id)
     {
-        //
+        $task = Task::find($id);
+        
+        return view('tasks.show', ['task' => $task,
+        ]);
     }
 
     /**
@@ -61,7 +72,10 @@ class TasksController extends Controller
      */
     public function edit($id)
     {
-        //
+        $task = Task::find($id);
+        
+        return view('tasks.edit', ['task'=>$task
+        ]);
     }
 
     /**
@@ -73,7 +87,11 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+       $task = Task::find($id);
+       $task->content = $request->content;
+       $task->save();
+       
+       return redirect('/');
     }
 
     /**
@@ -84,6 +102,9 @@ class TasksController extends Controller
      */
     public function destroy($id)
     {
-        //
+       $task = Task::find($id);
+       $task->delete();
+       
+       return redirect ('/');
     }
 }
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -15,12 +15,13 @@
             <tbody>
                 @foreach ($tasks as $task)
                 <tr>
-                    <td>{{ $task->id }}</td>
+                    <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
                     <td>{{ $task->content }}</td>
                 </tr>
                 @endforeach
             </tbody>
         </table>
+        {!! link_to_route('tasks.create', '新規メッセージの投稿', null, ['class' => 'btn btn-primary']) !!}
     @endif
 
 @endsection
