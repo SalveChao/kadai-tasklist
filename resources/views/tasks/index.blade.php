b2c719b04e4bfae38dba632c3c6fa2eb0d07e159
salve.chao <salve-chao@live.jp>
2019-07-15 03:34:55 +0000

    modifying view files

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -3,27 +3,29 @@
 @section('content')
 
 <h1>メッセージ一覧</h1>
-
-    @if (count($tasks) > 0)
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                    <th>id</th>
-                    <th>Status</th>
-                    <th>メッセージ</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($tasks as $task)
-                <tr>
-                    <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
-                    <td>{{ $task->status }}</td>
-                    <td>{{ $task->content }}</td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
-        {!! link_to_route('tasks.create', '新規メッセージの投稿', null, ['class' => 'btn btn-primary']) !!}
+    @if (Auth::id())
+        @if (count($tasks) > 0)
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>id</th>
+                        <th>Status</th>
+                        <th>メッセージ</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($tasks as $task)
+                    <tr>
+                        <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
+                        <td>{{ $task->status }}</td>
+                        <td>{{ $task->content }}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+            {!! link_to_route('tasks.create', '新規メッセージの投稿', null, ['class' => 'btn btn-primary']) !!}
+        @endif
+    @else
+        @include('welcome')
     @endif
-
 @endsection
